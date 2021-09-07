a9f8ca1e27c453824860e136cfa28e97d18796fa
sondos zaro <197075@ppu.edu.ps>
2021-09-07 13:02:27 +0300

    React assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
+let ifTrue = true;
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+    const clickMe = () => {
+        console.log(ifTrue)
+        if (ifTrue) {
+            ifTrue = false
+            setText("Hello Palestine")
+
+        } else {
+            ifTrue = true;
+            setText("Hello World!")
+
+        }
+    }
+    const [text, setText] = useState("Hello World!")
+    return (< div className="App" >
+        <button onClick={clickMe}>{text}</button>
+
+    </div>
+    );
+}
+const WhenInputChange = () => {
+    const [userName, setUserName] = useState("user name");
+    const [email, setEmail] = useState("example@hotmail.com");
+    return (
+        <div className="App">
+            <p>{`Email : ${email}`}</p>
+            <p>{`User Name : ${userName}`}</p>
+            <input onChange={(event) => setUserName(event.target.value)} placeholder="username" />
+            <input onChange={(event) => setEmail(event.target.value)} placeholder="Email" />
+
+
+        </div>
+    )
+}
+const RandomSentence = () => {
+    const [sentence, setArr] = useState("Change Sentence")
+    const onClickButton = () => {
+        const sentenceArr = ["My name is Sondos",
+            "Hi everyone",
+            "How are you",
+            "are you Ok ?"
+            , "have a nice day",
+            "Hello World!",
+            "What's Your name",
+            "Leave a kind message ,Please ",
+            "this is a third assignment",
+            "last sentence"];
+        setArr(sentenceArr[Math.floor(Math.random() * sentenceArr.length)])
+    }
+    return (
+        <div className="App">
+            <button onClick={onClickButton} >{sentence}</button>
+        </div>
+    )
+}
+export { App, WhenInputChange, RandomSentence };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { App, WhenInputChange, RandomSentence } from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <WhenInputChange />
+    <RandomSentence />
   </React.StrictMode>,
   document.getElementById('root')
 );
